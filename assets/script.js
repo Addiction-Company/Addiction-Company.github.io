1a5cd54330854a0f6e3439260e748e445d456e40
Strange <krutonpl@gmail.com>
2020-11-09 18:13:24 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,18 @@ $(".navTrigger").click(() => {
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
+
+let container, details;
+window.addEventListener("load", () => {
+  container = document.querySelector("#projectsContainer");
+  details = container.parentElement.querySelector("#projectDetails");
+});
+function switchProject(newActive, detailSelector) {
+  let old = Array.from(container.querySelectorAll("a")).find((a) =>
+    a.classList.contains("current")
+  );
+  if (old) old.classList.remove("current");
+  newActive.classList.add("current");
+  Array.from(details.children).map((ch) => (ch.style.display = "none"));
+  details.querySelector(detailSelector).style.display = "block";
+}
